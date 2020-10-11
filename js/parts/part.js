2793747d32c2e6eb55f20359cef6d96c98a5f650
Pavel Shaposhnikov <s.pavel102@gmail.com>
2020-10-12 00:40:20 +0300

    Implement overflow detection

--- js/parts/part.js
+++ js/parts/part.js
@@ -1,31 +1,26 @@
 (function($) {
 
     function setInitDropdownPosition() {
-        $('.navbar-nav .dropdown-toggle').each(function() {
-            let dropdown = $(this).parent();
-            if (!dropdown.parent().hasClass('navbar-nav')) {
-                if ($(window).width() >= 992) {
-                    dropdown.removeClass('dropdown dropleft').addClass('dropright');
-                } else {
-                    dropdown.removeClass('dropleft dropright').addClass('dropdown');
-                }
-            }
-        });
-    }
-
-    function setDropdownPosition(dropdownToggle) {
-        if ($('.navbar-nav').data('detect-overflow')) {
-            let dropdown = dropdownToggle.parent();
-            let menu     = dropdownToggle.siblings('.dropdown-menu');
-            if (!dropdown.parent().hasClass('navbar-nav')) {
-                if (dropdown.is(':visible')) {
+        if ($(window).width() >= 992) {
+            $('.navbar-nav .dropdown-menu').css({visibility: 'hidden', display: 'block'});
+            $('.navbar-nav .dropdown-toggle').each(function() {
+                let dropdown = $(this).parent();
+                let menu     = $(this).siblings('.dropdown-menu');
+                if (!dropdown.parent().hasClass('navbar-nav')) {
                     let windowEdge   = $(window).width();
-                    let dropdownEdge = menu.offset().left + menu[0].scrollWidth;
-                    if (dropdownEdge > windowEdge) {
+                    let dropdownEdge = dropdown.offset().left + dropdown.outerWidth() + menu.outerWidth();
+                    if ($('.navbar-nav').data('detect-overflow') && dropdownEdge > windowEdge) {
                         dropdown.removeClass('dropdown dropright').addClass('dropleft');
+                    } else {
+                        dropdown.removeClass('dropdown dropleft').addClass('dropright');
                     }
                 }
-            }
+            });
+            $('.navbar-nav .dropdown-menu').css({display: '', visibility: ''});
+        } else {
+            $('.navbar-nav .dropdown-toggle').each(function() {
+                $(this).parent().removeClass('dropleft dropright').addClass('dropdown');
+            });
         }
     }
 
@@ -35,8 +30,6 @@
         $(this).parent().toggleClass('show');
         $(this).siblings('.dropdown-menu').toggleClass('show');
         $(this).siblings('.dropdown-menu').find('.show').toggleClass('show');
-
-        setDropdownPosition($(this));
     };
 
     let closeDropdown = function(e) {
@@ -65,13 +58,14 @@
     $(window).on('load resize orientationchange', function() {
         clearTimeout(resizeTimer);
         resizeTimer = setTimeout(function() {
+            setInitDropdownPosition();
             resetNavbar();
             bindNavbar();
-            setInitDropdownPosition();
             if ($('.navbar-nav').data('hover') === true && $(window).width() >= 992) {
                 unbindNavbar();
             }
         }, 250);
     });
+
 
 })(jQuery);
